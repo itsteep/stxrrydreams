aefbd0dec1a3fb0f2846ace0a7afcf114d9d1222
itsteep <97403425+itsteep@users.noreply.github.com>
2023-04-18 08:58:42 -0500

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,21 @@
-const toggleButton = document.getElementById("toggle-button"); 
-const naviList = document.getElementById("navi-list"); 
+var btnToggle = true; 
 
-console.log("toggle"); 
-toggleButton.addEventListener("click", function () {
-    console.log("menu clicked");
-    naviList.classList.toggle("active"); 
-})
+const btn = document.getElementById("gal-btn"); 
+const text = document.getElementById("gal-text"); 
+
+function chClr() {
+    if (btnToggle) {
+        btn.textContent = "Revert Changes!"; 
+        document.body.style.backgroundColor = "#c0e1bd"; 
+        text.style.color = "#081196"; 
+        btnToggle = false; 
+        btn.style.backgroundColor = "#fed0d2"; 
+    }
+    else { 
+        document.body.style.backgroundColor = "white"; 
+        text.style.color = "#000"; 
+        btnToggle = true; 
+        btn.style.backgroundColor = "#fff"; 
+        btn.textContent = "Change Colors!"; 
+    }
+}
